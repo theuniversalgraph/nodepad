de21a6cbd87c04acc7961dd51487faaec475177b
toukubo <toukubo@gmail.com>
2015-03-31 04:39:40 +0900

    copy core jar shell, that works for https://basecamp.com/2779900/projects/8773061/todos/166115335#events_todo_166115335

--- nodepad/mda/copycorejar.sh
+++ nodepad/mda/copycorejar.sh
@@ -1,5 +1,6 @@
 copy .\web\target\storyteller-web-1.0\WEB-INF\lib\storyteller-core-1.0.jar ..\WEB-INF\lib
 copy .\web\target\storyteller-web-1.0\WEB-INF\lib\storyteller-common-1.0.jar ..\WEB-INF\lib
-java -cp "jp.storyteller.desktop.jar;../WEB-INF/lib/commons-httpclient-3.0.1.jar;../WEB-INF/lib/commons-logging.jar;../WEB-INF/lib/commons-codec-1.3.jar"  net.storyteller.desktop.HttpClientFilePostOfAndromdaCoreJarAddingToJ2eeStory http://localhost:8080/storyteller /Users/toukubo/Dropbox/git/storyteller/ storyteller
-wget http://localhost:8080/st/SystemCreatesNonnsAndAttrsFromCoreJarOfJ2eeStorys.do
-java -cp "jp.storyteller.desktop.jar;../WEB-INF/lib/commons-httpclient-3.0.1.jar;../WEB-INF/lib/commons-logging.jar;../WEB-INF/lib/commons-codec-1.3.jar"  net.storyteller.desktop.StorytellerCodesDownload http://localhost:8080/st c:/current/eclipse/workspace/ storyteller
+java -cp "jp.storyteller.desktop.jar:commons-httpclient-3.0.1.jar:commons-logging.jar:commons-codec-1.3.jar"  net.storyteller.desktop.HttpClientFilePostOfAndromdaCoreJarAddingToJ2eeStory http://localhost:8080/storyteller /Users/toukubo/Dropbox/git/ nodepad
+curl -X GET "http://localhost:8080/storyteller/SystemCreatesNonnsAndAttrsFromCoreJarOfJ2eeStorys.do?j2eeStory=nodepad&packageName=enclosing%2Fapplication%2Fnode"
+
+# java -cp "jp.storyteller.desktop.jar:commons-httpclient-3.0.1.jar:commons-logging.jar:commons-codec-1.3.jar"  net.storyteller.desktop.StorytellerCodesDownload http://localhost:8080/storyteller /Users/toukubo/Dropbox/git/ nodepad
