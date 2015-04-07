77c0b5bb1f16ea9ec9d681a37f8215b76fab952f
toukubo <toukubo@gmail.com>
2015-04-07 18:51:18 +0900

    package refactoring sometimes occur visibility issues yeah. fixed.

---
+++ nodepad/src/main/java/enclosing/faceless/GetFacelessNodeField.java
@@ -0,0 +1,57 @@
+package enclosing.faceless;
+
+import java.io.File;
+import java.util.Vector;
+
+import enclosing.application.node.NodeFieldApplet;
+import enclosing.application.node.NodeObserver;
+import enclosing.application.node.NodeUtils;
+import enclosing.application.node.TestCounter;
+
+public class GetFacelessNodeField {
+	public static NodeFieldApplet initApplet() {
+		NodeFieldApplet applet = new NodeFieldApplet();
+		applet.setNet(false);
+		applet.setObserver(new NodeObserver(applet,null,null));
+		applet.getObserver().setCounter(new TestCounter());
+		applet.getObserver().setNodeFieldApplet(applet);
+		applet.getObserver().setFaceless(true);;
+		
+		if(applet.getObserver()==null){
+			try {
+				Thread.sleep(500);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+		return applet;
+	}
+
+	public static File getBasedirFile(String foundNodeContent) {
+		String basedir = "./data/";
+		if(isNodeSentence(foundNodeContent)){
+			basedir += "sentences/";
+		}
+		File basedirFile = new File(basedir);
+		return basedirFile;
+	}
+	public static boolean isNodeSentence(String content) {
+		if(getWords(content).length<3)
+			return false;
+		if(content.length()>20){
+			return true;
+		}
+		return true;
+	}
+
+
+	// there is a case we use underbar...
+	public static String[] getWords(String content) {
+		Vector<String> vector = new Vector<String>();
+		final String saferString = NodeUtils.saferStringOf(content);
+		String[] words = saferString.split("_");
+		System.err.println(words.length + " ----------- is the length of the words!");
+		return words;
+	}
+}
