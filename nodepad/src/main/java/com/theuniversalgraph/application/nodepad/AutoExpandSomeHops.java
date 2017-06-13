a2323ab5da69645c756ae421483af11e178cdabd
TOKUBOKOJI <toukubo@gmail.com>
2017-06-13 17:10:03 +0900

    complication error

---
+++ nodepad/src/main/java/com/theuniversalgraph/application/nodepad/AutoExpandSomeHops.java
@@ -0,0 +1,14 @@
+package com.theuniversalgraph.application.nodepad;
+
+import java.util.Hashtable;
+
+import enclosing.application.node.suggestion.AutoExpandOneStep;
+
+public class AutoExpandSomeHops {
+	int hopCount = 3;
+	public AutoExpandSomeHops(String wikistring,NodeComponent  expandedNodeComponent,Hashtable existingNodeComponents){
+//		AutoExpandOneStep firstStep = new AutoExpandOneStep(wikistring, expandedNodeComponent, existingNodeComponents);
+//		firstStep.
+		
+	}
+}
